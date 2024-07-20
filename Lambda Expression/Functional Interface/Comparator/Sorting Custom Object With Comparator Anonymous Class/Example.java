34cabde44311e8d8b0ae0e1f603c24740fac5fd1
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-07-20 19:19:02 +0530

    Update Example.java

--- Lambda Expression/Functional Interface/Comparator/Sorting Custom Object With Comparator Anonymous Class/Example.java
+++ Lambda Expression/Functional Interface/Comparator/Sorting Custom Object With Comparator Anonymous Class/Example.java
@@ -31,21 +31,31 @@ public class Example {
         people.add(new Person("Charlie", 35));
 
         Comparator < Person > ageComparator = new Comparator < Person > () {
+
             @Override
             public int compare(Person p1, Person p2) {
-                return Integer.compare(p1.getAge(), p2.getAge());
-                        // Or
-                //return  p1.getAge()-p2.getAge();
-                       //Or
-                // if (p1.getAge() == p2.getAge()) {
-                //     return 0;  //Do not swap the two object
-                // } else if (p1.getAge() > p2.getAge()) {
-                //     return 1;  //Swap the two object
-                // } else {
-                //     return -1; //Do not swap the two object
-                // }
-                      
+                if (p1.getAge() == p2.getAge()) {
+                    return 0; //Do not swap the two object
+                } else if (p1.getAge() > p2.getAge()) {
+                    return 1; //Swap the two object
+                } else {
+                    return -1; //Do not swap the two object
+                }
             }
+                            //OR
+            // @Override
+            // public int compare(Person p1, Person p2) {
+            //     return Integer.compare(p1.getAge(), p2.getAge());
+
+            // }
+
+                             //OR
+            // @Override
+            // public int compare(Person p1, Person p2) {
+            //     return  p1.getAge()-p2.getAge();
+
+            // }
+
         };
 
         Collections.sort(people, ageComparator);
