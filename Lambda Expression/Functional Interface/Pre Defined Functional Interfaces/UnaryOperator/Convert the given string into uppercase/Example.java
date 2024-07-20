70152117a82ce8bc8584fe18feaae5fae57dead4
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-07-20 16:27:03 +0530

    Update Example.java

--- Lambda Expression/Functional Interface/Pre Defined Functional Interfaces/UnaryOperator/Convert the given string into uppercase/Example.java
+++ Lambda Expression/Functional Interface/Pre Defined Functional Interfaces/UnaryOperator/Convert the given string into uppercase/Example.java
@@ -1,8 +1,8 @@
 import java.util.function.*;
 public class Example {
     public static void main(String[] args) {
-        UnaryOperator < String > lengthOfString = str - > str.toUpperCase();
-        String result = lengthOfString.apply("saurabh");
+        UnaryOperator < String > upperCaseString = str - > str.toUpperCase();
+        String result = upperCaseString.apply("saurabh");
         System.out.println(result);
     }
 }
