9c44eae4c0de4e1d77c2d594997ce8d21f427200
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-07-20 15:09:21 +0530

    Update Example.java

--- Lambda Expression/Functional Interface/Pre Defined Functional Interfaces/Predicate/Check If the given number is even or not/Example.java
+++ Lambda Expression/Functional Interface/Pre Defined Functional Interfaces/Predicate/Check If the given number is even or not/Example.java
@@ -2,6 +2,7 @@ import java.util.function.*;
 public class Example {
     public static void main(String[] args) {
         Predicate < Integer > isEven = num - > (num % 2 == 0);
-        System.out.println(isEven.test(9));
+        boolean result = isEven.test(8);
+        System.out.println(result);
     }
 }
