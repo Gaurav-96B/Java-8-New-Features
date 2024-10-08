409f1964c5c2e156302e2134cfb117fab27089e0
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-10-09 00:21:11 +0530

    Create Bank.java

---
+++ OOPS Concept/Interface/Bank.java
@@ -0,0 +1,19 @@
+interface Bank {
+    float rateOfInterest();
+}
+class SBI implements Bank {
+    public float rateOfInterest() {
+        return 9.15 f;
+    }
+}
+class PNB implements Bank {
+    public float rateOfInterest() {
+        return 9.7 f;
+    }
+}
+class TestInterface2 {
+    public static void main(String[] args) {
+        Bank b = new SBI();
+        System.out.println("ROI: " + b.rateOfInterest());
+    }
+}
