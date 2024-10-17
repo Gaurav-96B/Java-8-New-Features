3bbcbf56aa0c3b91271c748aaf6cba4d0a5245cb
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-10-17 22:47:16 +0530

    Create UserDaoImpl.java

---
+++ Spring Framework/Spring Hibernate/Implementation/DAO/UserDaoImpl.java
@@ -0,0 +1,25 @@
+package com.example.dao;
+
+import com.example.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public class UserDaoImpl implements UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void save(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Override
+    public User findById(int id) {
+        return sessionFactory.getCurrentSession().get(User.class, id);
+    }
+}
