388b00ba8f5a9ad448ceaca829818f2effe2ce6f
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-11-19 23:31:49 +0530

    Update Main.java

--- MultiThreading/Dufferent cases of executing thread/Performing a single task from multiple thread/Main.java
+++ MultiThreading/Dufferent cases of executing thread/Performing a single task from multiple thread/Main.java
@@ -1,1 +1,23 @@
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Printing number: " + i);
+            try {
+                Thread.sleep(500); // Simulate a delay of 500ms between prints
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted!");
+            }
+        }
+    }
+}
 
+public class Main {
+    public static void main(String[] args) {
+      
+        MyThread thread1 = new MyThread();
+        thread1.start(); // Start the thread and execute the task
+      
+        MyThread thread2=new MyThread();
+        thread2.start();
+    }
+}
