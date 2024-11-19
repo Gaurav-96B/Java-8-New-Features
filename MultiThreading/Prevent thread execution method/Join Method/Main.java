11d313c63f3e0c35ec229e6f67c8cf1408663c16
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-11-19 12:04:23 +0530

    Create Main.java

---
+++ MultiThreading/Prevent thread execution method/Join Method/Main.java
@@ -0,0 +1,42 @@
+class MyThread extends Thread{
+    public void run() {
+       for(int i=0;i<10;i++){
+           try{
+           Thread.sleep(1000);
+           }
+           catch(InterruptedException interruptedException){
+               System.out.println(interruptedException);
+           }
+           System.out.println(i);
+       }
+    }
+}
+
+class Main {
+    public static void main(String[] args) throws InterruptedException {
+        
+        //Main Thread
+        System.out.println(Thread.currentThread().getName());
+        Thread.currentThread().setName("Saurav");
+        System.out.println(Thread.currentThread().getName());
+        System.out.println("Main thread is running");
+        System.out.println(Thread.currentThread().getPriority());
+        System.out.println(Thread.currentThread().isAlive());
+        
+        // User Thread
+        MyThread thread1=new MyThread();
+        thread1.start();
+        
+        //Main thread will wait for user thread to complete its task
+        thread1.join();
+        for(int i=0;i<10;i++){
+          try{
+          Thread.sleep(1000);
+          }
+          catch(InterruptedException interruptedException){
+              System.out.println(interruptedException);
+           }
+          System.out.println(i);
+      }
+    }
+}
