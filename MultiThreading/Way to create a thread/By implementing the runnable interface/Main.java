666937322f6701ab550a7fadda4382fbf9d6a35d
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-11-19 07:42:13 +0530

    Update Main.java

--- MultiThreading/Way to create a thread/By implementing the runnable interface/Main.java
+++ MultiThreading/Way to create a thread/By implementing the runnable interface/Main.java
@@ -4,6 +4,7 @@ class MyRunnable implements Runnable{
         System.out.println("Thread 2 is running using Runnable interface.");
     }
 }
+
 class Main {
     public static void main(String[] args) {
       Thread thread2=new Thread(new MyRunnable());
