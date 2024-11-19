baa2c25f593956db995ab8ba4c9f615045ea3d31
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-11-19 07:44:57 +0530

    Update Main.java

--- MultiThreading/Way to create a thread/Create three thread/Main.java
+++ MultiThreading/Way to create a thread/Create three thread/Main.java
@@ -1,1 +1,25 @@
-
+import java.lang.*;
+class MyThread extends Thread{
+    public void run(){
+        System.out.println("Thread 1 is running using Thread class.");
+    }
+}
+class MyRunnable implements Runnable{
+    public void run(){
+        System.out.println("Thread 2 is running using Runnable Interface.");
+    }
+}
+class Main {
+    public static void main(String[] args) {
+      MyThread thread1=new MyThread();
+      thread1.start(); 
+      
+      Thread thread2=new Thread(new MyRunnable());
+      thread2.start();
+      
+      Thread thread3=new Thread(()->{
+          System.out.println("Thread 3 is running using Lambda Expression.");
+      });
+      thread3.start();
+    }
+}
