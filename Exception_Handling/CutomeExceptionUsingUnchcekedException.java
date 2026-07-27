40422c0a7e58848926b09e7f924c1c314af26db7
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2026-07-27 15:16:30 +0530

    Update CutomeExceptionUsingUnchcekedException.java

--- Exception_Handling/CutomeExceptionUsingUnchcekedException.java
+++ Exception_Handling/CutomeExceptionUsingUnchcekedException.java
@@ -1,1 +1,17 @@
-
+class InvalidAgeException extends RuntimeException{
+    public InvalidAgeException(String message){
+        super(message);
+    }
+}
+class AgeVerification{
+    public void getValidAge(int age){
+        if(age<20){
+            throw new InvalidAgeException("Age is invalid");
+        }
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        new AgeVerification().getValidAge(16);
+    }
+}
